35e9d00e3d251ee1146e42cafe3d0f003f76a296
Hana00661 <eng.hana95@gmail.com>
2024-03-23 05:19:11 +0400

    m

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
 #!/user/bin/pup
-# puppet declarative script to install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
-}
-package { 'Werkzeug':
-  ensure   => '2.1.0',
-  provider => 'pip3'
-}
+exec { 'install flask':
+  command => 'sudo pip3 install flask==2.1.0',
+  path    => '/usr/bin',
+  }
