14d98c2503084fc1bc0517f32c94bcc263a18750
Hana00661 <eng.hana95@gmail.com>
2024-03-23 04:34:05 +0400

    l

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Create a file in /tmp.
-file { 'school':
-  ensure  => 'file',
+file { '/tmp/school':
+  ensure  => 'present',
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
