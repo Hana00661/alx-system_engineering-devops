f95cfcb5d8b700eff33d3a9ea40a1e242c724cb4
Hana00661 <eng.hana95@gmail.com>
2024-03-26 19:29:31 +0400

    3

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,7 @@
+# Installs and configure an Nginx server using Puppet instead of Bash
+# Nginx should be listening on port 80
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/luischaparroc permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
