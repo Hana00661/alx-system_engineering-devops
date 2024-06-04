ab8f7124f9d09bc62c9ceb258107ad922a07e56c
Hana00661 <eng.hana95@gmail.com>
2024-06-04 23:18:53 +0400

    error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,20 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Function that queries the Reddit API """
-    user_agent = {'User-agent': 'comels'}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    req = requests.get(url, headers=user_agent, allow_redirects=False)
-    if req.status_code != 404:
-        return req.json()['data']['subscribers']
+
+    base_url = 'https://www.reddit.com/r/'
+
+    url = '{}{}/about.json'.format(base_url, subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    results = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
+    if results.status_code == 200:
+        return results.json()['data']['subscribers']
     return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,15 +7,23 @@ import requests
 def top_ten(subreddit):
     """ Function that prints the titles """
 
-    user_agent = {'User-agent': 'comeles'}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    req = requests.get(url,
-                       headers=user_agent,
-                       allow_redirects=False,
-                       params={"limit": 10})
-    if req.status_code != 404:
-        children = req.json()['data']['children']
-        for title in children:
-            print(title['data']['title'])
+    base_url = 'https://www.reddit.com'
+    sort = 'top'
+    limit = 10
+    url = '{}/r/{}/.json?sort={}&limit={}'.format(
+        base_url, subreddit, sort, limit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    response = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
+    if response.status_code == 200:
+        for post in response.json()['data']['children'][0:10]:
+            print(post['data']['title'])
     else:
-        print("None")
+        print(None)
